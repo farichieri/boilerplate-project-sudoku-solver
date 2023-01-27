aa493b51484c059f7e1a91ac62b5ad72caa6ad5c
Fabricio Richieri <fabriciorichieri@yahoo.com>
2023-01-27 13:24:51 -0300

    solve

--- routes/api.js
+++ routes/api.js
@@ -3,16 +3,38 @@
 const SudokuSolver = require('../controllers/sudoku-solver.js');
 
 module.exports = function (app) {
-  
   let solver = new SudokuSolver();
 
-  app.route('/api/check')
-    .post((req, res) => {
+  app.route('/api/check').post((req, res) => {
+    const { puzzle, coordinate, value } = req.body;
+  });
 
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
+  app.route('/api/solve').post((req, res) => {
+    const { puzzle } = req.body;
 
-    });
+    if (!puzzle) {
+      return res.status(200).send({ error: 'Required field missing' });
+    }
+
+    if (puzzle.length !== 81) {
+      return res
+        .status(200)
+        .send({ error: 'Expected puzzle to be 81 characters long' });
+    }
+
+    const regex = /[^0-9.]/g;
+    const matches = !regex.test(puzzle);
+
+    if (matches !== true) {
+      return res.status(200).send({ error: 'Invalid characters in puzzle' });
+    }
+
+    const solution = solver.solve(puzzle);
+
+    if (solution) {
+      return res.status(200).send({ solution: solution });
+    } else if (!solution) {
+      return res.status(200).send({ error: 'Puzzle cannot be solved' });
+    }
+  });
 };
